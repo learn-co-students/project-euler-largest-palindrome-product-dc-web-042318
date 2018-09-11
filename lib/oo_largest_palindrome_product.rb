718b4e0cdfc31924ad19339aea959183ceecb9bb
munirkizilcali <munir.kizilcali@gmail.com>
2018-09-10 22:30:16 -0400

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+	def initialize
+		@palindromes = []
+		for i in 1..100 do 
+			for j in 1..100 do
+				if self.is_palindrome((1000-i)*(1000-j))
+					@palindromes.push((1000-i)*(1000-j))
+				end
+			end
+		end
+	end
+
+	def is_palindrome(x)
+		x.to_s == x.to_s.reverse
+	end
+
+
+
+	def answer
+		@palindromes.sort.reverse[0]
+	end
+end
